a449cdcec27bf565bce1e5c25b7b853e3cebb04e
Roberto Ortega <berto.ort@gmail.com>
2017-11-20 11:26:43 -0800

    ✨ display teams from server

--- client/src/components/Teams.vue
+++ client/src/components/Teams.vue
@@ -1,6 +1,12 @@
 <template>
   <main>
     <h1> Teams </h1>
+    <h2 v-if="loading">Loading...</h2>
+    <h2 v-if="error">Oh no! We couldn't load the teams. Try again later</h2>
+    <section class="profile" v-for="team in teams">
+      <img :src="team.image_url" :alt="team.name"/>
+      <h3>{{team.name}}</h3>
+    </section>
   </main>
 </template>
 
@@ -9,10 +15,22 @@ import config from '../config';
 
 export default {
   name: 'Teams',
-  mounted: () => {
+  data: () => ({
+    teams: [],
+    loading: true,
+    error: false,
+  }),
+  mounted() {
     fetch(`${config.SERVER_URL}/api/teams`)
       .then(response => response.json())
-      .then(console.log);
+      .then((teams) => {
+        this.loading = false;
+        this.teams = teams;
+      })
+      .catch(() => {
+        this.loading = false;
+        this.error = true;
+      });
   },
 };
 </script>
